8e78352088f70c6bd99d06cdc18206692755bd9d
Thrishneshvarr Nageen Anantharajan <thrish81@gmail.com>
2022-02-26 13:48:33 +0800

    interaction panel

---
+++ Space Terminator/Assets/thrish/leverSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class leverSystem : MonoBehaviour
+{
+
+    public bool isColliding;
+    public GameObject Image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.GetComponent<UnitManager>())
+        {
+
+            Image.SetActive(true);
+            Debug.Log("Press 'E' ");
+            isColliding = true;
+            
+        }
+        else
+        {
+            isColliding = false;
+        }
+
+    }
+
+}
