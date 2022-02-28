a948fe7340882c0c1fac8ceda86fdc25ea2ac79e
pinkprosciutto <74938181+pinkprosciutto@users.noreply.github.com>
2022-02-28 21:40:31 +0800

    damage Text

--- Space Terminator/Assets/Jeremy Shoot Scene/Scripts/Combat/FloatingText.cs
+++ Space Terminator/Assets/Jeremy Shoot Scene/Scripts/Combat/FloatingText.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     [SerializeField] float destroyTime = 3f;
+    [SerializeField] Vector3 offset = new Vector3(0, 1, 0);
+    [SerializeField] Vector3 random = new Vector3(0.5f, 0, 0);
+
     void Start()
     {
+        transform.parent = null;
         Destroy(gameObject, destroyTime);
-    }
+        transform.rotation = Quaternion.Euler(50, 0, 0);
+        transform.localPosition += offset;
+        transform.localPosition += new Vector3(Random.Range(-random.x, random.x),
+            Random.Range(-random.y, random.y),
+            Random.Range(-random.z, random.z));
 
-    void Update()
-    {
-        
+
     }
 }
