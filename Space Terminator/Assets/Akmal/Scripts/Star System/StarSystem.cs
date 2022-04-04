5f8cecd41095e16f89cfed8580dbcaf2f15948b0
AkmalZulkifli <akmalzulkifli08@gmail.com>
2022-04-04 15:14:42 +0800

    Star System

---
+++ Space Terminator/Assets/Akmal/Scripts/Star System/StarSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSystem : MonoBehaviour
+{
+    public GameObject[] stars;
+    private int objectiveCounts;
+
+    void Start()
+    {
+        objectiveCounts = GameObject.FindGameObjectsWithTag("objectives").Length;
+    }
+
+    public void StarsAchieved()
+    {
+        int objectiveLeft = GameObject.FindGameObjectsWithTag("objectives").Length;
+        int objectiveCollected = objectiveCounts - objectiveLeft;
+
+        float percentage = float.Parse(objectiveCollected.ToString()) / float.Parse(objectiveCounts.ToString()) * 100f;
+
+        if(percentage >= 33f && percentage < 66)
+        {
+            stars[0].SetActive(true);
+        }
+        else if (percentage >= 66 && percentage < 70)
+        {
+            stars[0].SetActive(true);
+            stars[1].SetActive(true);
+        }
+        else
+        {
+            stars[0].SetActive(true);
+            stars[1].SetActive(true);
+            stars[2].SetActive(true);
+        }
+    }
+}
