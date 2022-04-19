4e12191c89d14316d1684fed54c63f8027f4defd
Thrishneshvarr Nageen Anantharajan <thrish81@gmail.com>
2022-04-19 22:15:29 +0800

    UI menu and in-game update

--- Space Terminator/Assets/Evincent/Scripts/MenuScripts.cs
+++ Space Terminator/Assets/Evincent/Scripts/MenuScripts.cs
@@ -46,4 +46,10 @@ public class MenuScripts : MonoBehaviour
             SceneManager.LoadScene(2);
         }
     }
+
+    public void BckMainMenu()
+    {
+        SceneManager.LoadScene("LobbyMenu");
+    }
+
 }
